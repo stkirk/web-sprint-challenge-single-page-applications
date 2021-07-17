101cd5cb36e171b6483bffe68d5a5d1e072dd45c
stkirk <shaanekirk@gmail.com>
2021-07-17 15:26:36 -0400

    installed styled components dependency, imported BrowserRouter and wrapped App component in index.js, added PizzaForm and Confirmation files to components dir, imported Route Link NavLink and Switch to app, scaffolded App JSX with parent div.app and nav with h1 and navLinks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+//imports from react-router-dom
+import { Route, Link, NavLink, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <nav>
+        <h1 className="store-header">Lambda Eats</h1>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          {/* maybe change Help link later PLACEHOLDER */}
+          <Link to="/">Help</Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 export default App;
