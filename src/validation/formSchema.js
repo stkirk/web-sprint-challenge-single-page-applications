31d48d95ea0addc5ed4d91cfec2878735abc9fec
stkirk <shaanekirk@gmail.com>
2021-07-17 20:16:23 -0400

    installed yup, made validation dir, created formSchema file, imported yup and wrote validation schema for formValues state

---
+++ src/validation/formSchema.js
@@ -0,0 +1,15 @@
+//import yup
+import * as yup from "yup";
+
+export default yup.object.shape({
+  name: yup
+    .string()
+    .required("Name Required")
+    .min(2, "name must be at least 2 characters"),
+  size: yup.string().required("Please choose a size"),
+  pepperoni: yup.boolean(),
+  sausage: yup.boolean(),
+  onions: yup.boolean(),
+  greenpeppers: yup.boolean(),
+  special: yup.string(),
+});
